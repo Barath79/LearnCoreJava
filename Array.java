44166888d2c89004a61e64aac8dc6f657eb4a59b
Barath79 <41343340+Barath79@users.noreply.github.com>
2018-07-17 23:40:13 -0400

    str 1

--- Array.java
+++ Array.java
@@ -6,19 +6,19 @@ public class Array {
 		
 		int[] intarray; //int array
 		intarray = new int[10];
-		String[] strarray;  //str array
-		strarray = new String[10];
+		String[] strarray1;  //str array
+		strarray1 = new String[10];
 		
 		for(int i=0;i<10;i++) {
 			intarray[i] = i;
-			strarray[i] = "abc";
+			strarray1[i] = "abc";
 		}
 		
 		
 		for(int i=0;i<10;i++)
-			System.out.println(intarray[i] + "  " + strarray[i]);
+			System.out.println(intarray[i] + "  " + strarray1[i]);
 		for(int i=0;i<10;i++)
-			System.out.println(intarray[i] + "  " + strarray[i]);
+			System.out.println(intarray[i] + "  " + strarray1[i]);
 			}
 	}
 
